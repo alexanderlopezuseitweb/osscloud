97be4e35dca6931d28d30791a4c62b119eba63b1
Andrés Meza Escallón <andresmeza@useitweb.com>
2016-06-14 17:57:06 -0500

    Test cases implemented for list tenants and create tenants in openstack using the authentication settings loaded from the Django database.

--- ossportal/oslayer/openstack/api.py
+++ ossportal/oslayer/openstack/api.py
@@ -3,24 +3,30 @@ from keystoneauth1 import session
 from keystoneclient.v3 import client
 from keystoneauth1.exceptions.http import Unauthorized
 
+from oslayer.models import Setting
+
 class KeystoneClientFactory:
     """Factory for OpenStack Keystone Authentication client."""
     sess = None
         
     def __init__(self):
-        
-        auth = v3.Password(auth_url='http://192.168.1.58:5000/v3',
-                            username='ossportal', 
-                            password='ossportal1', 
-                            project_name='admin',
-                            user_domain_name='default',
-                            project_domain_id='default')
+        """Sets authorization credentials to start a new session as admin user in Openstack Keystone."""
+        # Load connection credentials from settings database  
+        cs = self.__loadSettings__()
+        auth = v3.Password(auth_url=cs['auth_url'],
+                            username=cs['username'], 
+                            password=cs['password'], 
+                            project_name=cs['project_name'],
+                            user_domain_name=cs['user_domain_name'],
+                            project_domain_id=cs['project_domain_id'])
+        # Set session with authorization credentials 
         self.sess = session.Session(auth=auth)
         
     def create_client(self):
         """Creates the new keystone client."""
+        # Set Keystone client based on session
         keystoneClient = client.Client(session=self.sess)
-        
+        # Validate that the Keystone client is working.
         try:
             headers = keystoneClient.session.get_auth_headers()  # @UnusedVariable
         except Unauthorized, Argument: 
@@ -28,3 +34,14 @@ class KeystoneClientFactory:
             raise "Authentication failed", Argument
         
         return keystoneClient
+    
+    def __loadSettings__(self):
+        """Loads settings values from database."""
+        # Define settings values with configuration from database
+        connection_settings = {'auth_url':Setting.objects.get(id='auth_url').value,
+                            'username':Setting.objects.get(id='username').value, 
+                            'password':Setting.objects.get(id='password').value, 
+                            'project_name':Setting.objects.get(id='project_name').value,
+                            'user_domain_name':Setting.objects.get(id='user_domain_name').value,
+                            'project_domain_id':Setting.objects.get(id='project_domain_id').value}
+        return connection_settings
--- ossportal/oslayer/tests.py
+++ ossportal/oslayer/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
 
+from .models import Setting
+from openstack import api
 # Create your tests here.
+
+class OpenStackTests(TestCase):
+    ks_client = None
+    
+    def setUp(self):
+        """Create connection settings to be used in further tests."""
+        TestCase.setUp(self)
+        Setting.objects.create(id='auth_url', value='http://192.168.1.58:5000/v3')
+        Setting.objects.create(id='username', value='ossportal')
+        Setting.objects.create(id='password', value='ossportal1')
+        Setting.objects.create(id='project_name', value='admin')
+        Setting.objects.create(id='user_domain_name', value='default')
+        Setting.objects.create(id='project_domain_id', value='default')
+        kcf = api.KeystoneClientFactory()
+        self.ks_client = kcf.create_client()
+         
+    
+    def test_connection_settings_in_database(self):
+        """there must be a set of keystone authorization 
+        settings defined in the database."""
+        self.assertGreaterEqual(len(Setting.objects.all()), 6)
+        self.assertIsNotNone(Setting.objects.get(id='auth_url'))
+        self.assertIsNotNone(Setting.objects.get(id='username'))
+        self.assertIsNotNone(Setting.objects.get(id='password'))
+        self.assertIsNotNone(Setting.objects.get(id='project_name'))
+        self.assertIsNotNone(Setting.objects.get(id='user_domain_name'))
+        self.assertIsNotNone(Setting.objects.get(id='project_domain_id'))
+        
+    def test_access_to_tenants(self):
+        """There must be at least three default tenants in Openstack"""
+        self.assertGreaterEqual(len(self.ks_client.projects.list()), 3)
+        
+    def test_create_tenant(self):
+        """A new tenant must be created"""
+        self.ks_client.projects.create(name='test_tenant', domain='default', description='This is a test from oslayer.')
+        ten = None
+        for t in self.ks_client.projects.list():
+            if t.name == 'test_tenant':
+                ten = t
+                break
+            
+#        ten = self.ks_client.projects.get('test_tenant')
+        self.assertIsNotNone(ten)
+        self.ks_client.projects.delete(ten)           
+        
+        
+        
+                     
